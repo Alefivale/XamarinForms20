f4b2bf0fe85e9393051208e6e4d42b3344c52dd0
Alefi <alefi.guitarra@gmail.com>
2020-04-27 13:57:54 -0300

    App_cep

---
+++ App_CEP/App_CEP/App_CEP/Servico/ViaCEPServico.cs
@@ -0,0 +1,27 @@
+﻿using App_CEP.Servico.Modelo;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace App_CEP.Servico
+{
+    public class ViaCEPServico
+    {
+        public static string EnderecoURL = "http://viacep.com.br/ws/{0}/json/";
+        public static Endereco BuscarEnderecoViaCEP(string cep)
+        {
+            string NovoEnderecoURL = string.Format(EnderecoURL, cep);
+
+            WebClient wc = new WebClient();
+            string Conteudo = wc.DownloadString(NovoEnderecoURL);
+
+            Endereco end = JsonConvert.DeserializeObject<Endereco>(Conteudo);
+
+            if (end.cep == null) return null;
+
+            return end;
+        }
+    }
+}
